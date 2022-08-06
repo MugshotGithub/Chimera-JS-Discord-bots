807e2f4c40d2880db6a3a665ca7b0ffbe4ea0fad
Oliver <106947072+MugshotGithub@users.noreply.github.com>
2022-08-06 12:43:23 +1200

    Initial Commit
    Functional bots for both Chimera Hell Let Loose servers

---
+++ server2.js
@@ -0,0 +1,47 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config2.json');
+const Gamedig = require('gamedig');
+
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const gamedig = new Gamedig();
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	gamedig.query({
+        socketTimeout: 2000,
+        attemptTimeout: 10000,
+        maxAttempts: 1,
+        protocol: 'valve',
+        type: 'hll',
+        host: '139.99.148.108',
+        port : '26420',
+        debug: false
+    }).then((state) => {
+        client.user.setActivity(String(" on " + state.map + " - " +state.players.length + "/" + state.maxplayers), {type: "PLAYING"});
+    }).catch((error) => {
+       console.log(error);
+    });
+});
+
+setInterval(function() {
+    getServerInfo();
+  }, 60000);
+
+function getServerInfo(){
+    gamedig.query({
+        type: 'hll',
+        host: '139.99.148.108',
+        port : '26420',
+    }).then((state) => {
+        client.user.setActivity(String(" on " + state.map + " - " +state.players.length + "/" + state.maxplayers), {type: "PLAYING"});
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+// Login to Discord with your client's token
+client.login(token);
+
